47ad0a6fcd8bb81ceaf3ee02760b15aa5ff5878f
Bárbara Melo <ibarbaramelo@gmail.com>
2026-03-12 11:01:03 -0300

    Incluindo mais exemplos

--- mensagem_usuario.py
+++ mensagem_usuario.py
@@ -5,5 +5,13 @@ Usando o input para interação com usuario
 Saida de dados com o print
  '''
 
+print('Olá, tudo bem?!')
+print
+
+
 nome = input('Digite o seu nome: ')
 print(f'Seja bem-vindo {nome}. É um prazer ter você aqui!!')
+
+
+idade = int(input('Digite sua idade: '))
+print(f'Você tem {idade} anos, certo?')
