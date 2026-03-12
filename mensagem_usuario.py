9b2b1f2bd3cf56e1232f9bedcea88fe048b01615
Bárbara Melo <ibarbaramelo@gmail.com>
2026-03-12 10:45:02 -0300

    first commit

---
+++ mensagem_usuario.py
@@ -0,0 +1,9 @@
+'''
+Entrada dados: 
+Criando variavel
+Usando o input para interação com usuario
+Saida de dados com o print
+ '''
+
+nome = input('Digite o seu nome: ')
+print(f'Seja bem-vindo {nome}. É um prazer ter você aqui!!')
