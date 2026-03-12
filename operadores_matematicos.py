29839ec00150f94511f1872dce37c2fdd365b3ca
Bárbara Melo <ibarbaramelo@gmail.com>
2026-03-12 11:18:40 -0300

    operadores matematicos

---
+++ operadores_matematicos.py
@@ -0,0 +1,28 @@
+# Calculadora simples
+
+#soma
+
+numero1 = int(input('Digie um número para soma: '))
+numero2 = int(input('Digie outro número para soma: '))
+print(numero1 + numero2)
+
+
+#Subtração
+
+numero3 = int(input('Digite um número para subtração: '))
+numero4 = int(input('Digite outro número para subtração: '))
+print(numero3 - numero4)
+
+
+#Multiplicação
+
+numero5 = int(input('Digite um número para multiplicação: '))
+numero6= int(input('Digite outro número para multiplicação: '))
+print(numero5 * numero6)
+
+
+#Divisão
+
+numero7 = int(input('Digite um número para divisão: ' ))
+numero8 = int(input('Digite outro número para divisão: '))
+print(numero7 / numero8)
