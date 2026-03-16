283befabd7d8774187aff869784b89eeb1a9c27c
Bárbara Melo <ibarbaramelo@gmail.com>
2026-03-16 10:14:12 -0300

    Aprendendo sobre strings

---
+++ Strings .py
@@ -0,0 +1,15 @@
+#Strings de múltiplas linhas
+
+
+nome = "Guilherme"
+
+mensagem = f"""
+
+Olá meu nome é {nome}
+        Eu estou aprendendo Python
+    Essa mensagem tem diferentes recuos.
+"""
+
+print(mensagem)
+
+
