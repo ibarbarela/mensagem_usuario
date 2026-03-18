bca08ddd1c2560e9846d65beb7cad256075f9f77
Bárbara Melo <ibarbaramelo@gmail.com>
2026-03-18 11:51:16 -0300

    novos exercicios

---
+++ MEUS TESTE.py
@@ -0,0 +1,10 @@
+
+usuario = input('Digite o seu nome:')
+
+print(f'\nOlá {usuario}! Seja bem-vindo(a) a sua calculadora no Python :)\n\nDe inicio, vamos comecar com dois números')
+
+soma = float(input('Digite o valor de a: '))
+soma1 = float(input('Digite o valor de b: '))
+total = (soma + soma1) / 2
+print(f'A soma de {soma} + {soma1} dividido por 2 é igual a {total}')
+
---
+++ Variaveis-e-Tipos.py/exercicio3.py
@@ -0,0 +1,3 @@
+# EXERCÍCIO 2: formatador de Nomes (Foco: str e limpeza)
+# Instrução: Receba um nome que pode vir com espaços extras e corrija.
+
